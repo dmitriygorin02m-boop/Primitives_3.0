7bd924f7468d35d808133a9795a4b6bbc5092955
Dmitriy Groin <dmitriy.gorin.02m@gmail.com>
2026-04-18 12:09:20 +0300

    Changing the bonus terms

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        double balance = -10;
-       double refill = 1_099.09;
+        int balance = 100;
+       int refill = 1001;
 
-        double bonus;
-        if (refill >= 1_100) {
+        int bonus;
+        if (refill > 1000) {
             bonus = refill / 100;
 
         } else {
             bonus = 0;
         }
 
-        double result = bonus + balance + refill;
+        int result = bonus + balance + refill;
 
         System.out.println("Итоговый счёт: " + result);
 
